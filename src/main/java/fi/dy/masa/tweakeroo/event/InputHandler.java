b9d03e09b1fd3150d8ec59b6dc6411588888a20a
Andrew S <Andrews54757@gmail.com>
2022-07-31 16:35:17 -0400

    Fixes

--- src/main/java/fi/dy/masa/tweakeroo/event/InputHandler.java
+++ src/main/java/fi/dy/masa/tweakeroo/event/InputHandler.java
@@ -132,7 +132,7 @@ public class InputHandler implements IKeybindProvider, IKeyboardInputHandler, IM
                     for (int i = 0; i < offset; i++)
                     {
                         BlockHitResult context = new BlockHitResult(new Vec3d(hit.getBlockPos().getX(), hit.getBlockPos().getY(), hit.getBlockPos().getZ()),Direction.NORTH, hit.getBlockPos(), false);
-                        mc.interactionManager.interactBlock(mc.player, mc.world, Hand.MAIN_HAND, context);
+                        mc.interactionManager.interactBlock(mc.player, Hand.MAIN_HAND, context);
                     }
                     return true;
                 }
--- src/main/java/fi/dy/masa/tweakeroo/renderer/RenderUtils.java
+++ src/main/java/fi/dy/masa/tweakeroo/renderer/RenderUtils.java
@@ -703,40 +703,6 @@ public class RenderUtils
         buffer.vertex(maxX, maxY, maxZ).color(color.r, color.g, color.b, color.a).next();
     }
 
-    public static void fillRect(Matrix4f matrix, int x1, int y1, int x2, int y2, int color) {
-		int j;
-		if (x1 < x2) {
-			j = x1;
-			x1 = x2;
-			x2 = j;
-		}
-
-		if (y1 < y2) {
-			j = y1;
-			y1 = y2;
-			y2 = j;
-		}
-
-		float f = (float)(color >> 24 & 255) / 255.0F;
-		float g = (float)(color >> 16 & 255) / 255.0F;
-		float h = (float)(color >> 8 & 255) / 255.0F;
-		float k = (float)(color & 255) / 255.0F;
-		BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
-		RenderSystem.enableBlend();
-		RenderSystem.disableTexture();
-		RenderSystem.defaultBlendFunc();
-		RenderSystem.setShader(GameRenderer::getPositionColorShader);
-		bufferBuilder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR);
-		bufferBuilder.vertex(matrix, (float)x1, (float)y2, 0.0F).color(g, h, k, f).next();
-		bufferBuilder.vertex(matrix, (float)x2, (float)y2, 0.0F).color(g, h, k, f).next();
-		bufferBuilder.vertex(matrix, (float)x2, (float)y1, 0.0F).color(g, h, k, f).next();
-		bufferBuilder.vertex(matrix, (float)x1, (float)y1, 0.0F).color(g, h, k, f).next();
-		bufferBuilder.end();
-		BufferRenderer.draw(bufferBuilder);
-		RenderSystem.enableTexture();
-		RenderSystem.disableBlend();
-	}
-    
     public static void renderAreaSides(BlockPos pos1, BlockPos pos2, Color4f color, MatrixStack matrices, MinecraftClient mc)
     {
         RenderSystem.enableBlend();
